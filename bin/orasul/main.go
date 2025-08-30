8533c4f6b90bfb6032193d3c02051c98d76274bc
jnnkrdb <jannek.rodenburger@icloud.com>
2025-08-30 15:55:02 +0000

    added round counter

--- bin/orasul/main.go
+++ bin/orasul/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/jnnkrdb/orasul/bin/orasul/config"
+import (
+	"time"
+
+	"github.com/jnnkrdb/orasul/bin/orasul/config"
+	"github.com/jnnkrdb/orasul/pkg/logging"
+)
 
 func main() {
 
 	config.LoadConfig()
+
+	var round int = 1
+	for {
+		time.Sleep(5 * time.Minute)
+		logging.Default.Debug("Current round %d", round)
+		round++
+	}
 }
